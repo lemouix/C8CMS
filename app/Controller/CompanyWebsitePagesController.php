3b48b033fd78ec01e04dc0bea23b53d4fa5b1035
Aymeric Mathéossian <aymeric.matheossian@gmail.com>
2015-01-26 19:09:53 +0100

    Gestion des pages

---
+++ app/Controller/CompanyWebsitePagesController.php
@@ -0,0 +1,15 @@
+<?php
+class WebsitePagesController extends AppController {
+    
+    public function index($page_id = null)
+    {
+        $WebsitePage = $this->WebsitePage->findById($page_id);
+        if (!$WebsitePage) {
+            throw new NotFoundException('La page que vous demandez n\'existe pas');
+        }
+        $this->set('WebsitePage', $WebsitePage);
+    }
+   
+    
+}
+?>
